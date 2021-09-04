c06d6d589139713559481a078340b0cf1569d677
Farhan <farhan7011@hotmail.com>
2021-09-04 19:49:46 +0530

    Removed SCSS files

--- src/components/connection-groups/ConnectionGroups.js
+++ src/components/connection-groups/ConnectionGroups.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import './ConnectionGroups.scss';
 import Button from '../button/Button';
 
 const ConnectionGroups = ({ previousStep, connectionType, onlyMe, allUsers }) => {
