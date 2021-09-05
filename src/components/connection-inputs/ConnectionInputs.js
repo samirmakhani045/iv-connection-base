6e73334973bea5393703d9a8674b69d4cd96f9df
Samir Makhani <samir@kinnect.us>
2021-09-05 11:42:06 +0530

    en removed

--- src/components/connection-inputs/ConnectionInputs.js
+++ src/components/connection-inputs/ConnectionInputs.js
@@ -68,7 +68,7 @@ const ConnectionInputs = ({
       port: portValue,
     });
   };
-  const { t } = useTranslation();
+  const { t } = useTranslation('common');
   return (
     <>
       <InputWrapper>
@@ -80,7 +80,7 @@ const ConnectionInputs = ({
             value={nickNameValue}
             name="nickName"
             onChange={(e) => setNickName(e.target.value)}
-            label={t('en.common.forms.fields.nickName') + '*'}
+            label={t('forms.fields.nickName') + '*'}
             id="nickName"
           />
         </ConnectionDiv>
@@ -91,7 +91,7 @@ const ConnectionInputs = ({
             value={databaseNameValue}
             name="databaseName"
             onChange={(e) => setDatabaseName(e.target.value)}
-            label={t('en.common.forms.fields.databaseName')}
+            label={t('forms.fields.databaseName')}
             id="database"
           />
         </ConnectionDiv>
@@ -102,7 +102,7 @@ const ConnectionInputs = ({
             value={warehouseValue}
             name="warehouse"
             onChange={(e) => setWarehouse(e.target.value)}
-            label={t('en.common.forms.fields.warehouse')}
+            label={t('forms.fields.warehouse')}
             id="warehouse"
           />
         </ConnectionDiv>
@@ -113,7 +113,7 @@ const ConnectionInputs = ({
             value={serverValue}
             name="server"
             onChange={(e) => setServer(e.target.value)}
-            label={t('en.common.forms.fields.server')}
+            label={t('forms.fields.server')}
             id="server"
           />
         </ConnectionDiv>
@@ -124,7 +124,7 @@ const ConnectionInputs = ({
             value={portValue}
             name="port"
             onChange={(e) => setPort(e.target.value)}
-            label={t('en.common.forms.fields.port')}
+            label={t('forms.fields.port')}
             id="port"
             isNumber={true}
           />
@@ -139,22 +139,22 @@ const ConnectionInputs = ({
         </TestButton>
         {isShowSuccess && (
           <ConnectionMessage success={true}>
-            <Text success={true}>{t('en.common.forms.success.dbConnectionSuccess')}</Text>
+            <Text success={true}>{t('forms.success.dbConnectionSuccess')}</Text>
           </ConnectionMessage>
         )}
         {isShowError && (
           <ConnectionMessage success={false}>
-            <Text success={false}>{t('en.common.forms.errors.dbConnectionError.required')}</Text>
+            <Text success={false}>{t('forms.errors.dbConnectionError.required')}</Text>
           </ConnectionMessage>
         )}
       </InputWrapper>
       <StepFooter>
-        <Button variant="light" onClick={() => backStep()} label={t('en.common.button.back')} />
+        <Button variant="light" onClick={() => backStep()} label={t('button.back')} />
         <Button
           variant="dark"
           disabled={!isShowSuccess}
           onClick={() => forwardStep()}
-          label={t('en.common.button.next')}
+          label={t('button.next')}
         />
       </StepFooter>
     </>
